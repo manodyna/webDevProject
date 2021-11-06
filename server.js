67a5593d812051daf88bc110bfbd02bebddb198e
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2021-11-06 17:50:36 +0530

    ..

--- server.js
+++ server.js
@@ -2,40 +2,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/bmi", function(req, res){
-    res.sendFile(__dirname+"/bmi.html")
+app.get("/bmi", function (req, res) {
+    res.sendFile(__dirname + "/bmi.html")
 });
 
-app.post("/bmi.html", function(req, res){
+app.post("/bmi.html", function (req, res) {
     console.log("post initiated");
     var Weight = Number(req.body.weight);
     var Height = Number(req.body.height);
 
-    var bmi = Weight/(Height*Height);
+    var bmi = Weight / (Height * Height);
     var range;
-    if(bmi<19){
+    if (bmi < 19) {
         range = "undeweight";
-    }else if(bmi>19 && bmi<25){
+    } else if (bmi > 19 && bmi < 25) {
         range = "normal";
-    }else if(bmi>25 && bmi<30){
+    } else if (bmi > 25 && bmi < 30) {
         range = "overweight";
-    }else{
+    } else {
         range = "obese";
     }
 
     var bmiResponse = {
-        val : bmi,
+        val: bmi,
         comment: range
     }
     res.send(bmiResponse);
 });
 
-app.get("/google-fit-data", function(req, res){
+app.get("/google-fit-data", function (req, res) {
     res.send("google fit dash");
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("server started on port 3000");
 });
