3e2bc5fa6b44f3fc693da3efa0fcc18bbb3582ee
Manodyna K H <77208955+manodyna@users.noreply.github.com>
2021-11-09 08:47:18 +0530

    url parse installed google api integration in progress

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const
+const { google } = require("googleapis");
+const request = require("request");
+const urlParse = require("url-parse");
+const queryParse = require("query-string");
+const cors = require("cors");
+const axios = require("axios");
+const { json } = require("body-parser");
+const { response } = require("express");
 
+// 412564020124-qr6c7fsgafarqu442obmbtspl9e2k5p7.apps.googleusercontent.com
+// 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
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/bmi", function (req, res) {
     res.sendFile(__dirname + "/bmi.html")
@@ -34,9 +46,35 @@ app.post("/bmi.html", function (req, res) {
     res.send(bmiResponse);
 });
 
-app.get("/google-fit-data", function (req, res) {
-    res.send("google fit dash");
+app.get("/login", (req, res)=>{
+    const oauth2Client = new google.auth.OAuth2(
+        "412564020124-qr6c7fsgafarqu442obmbtspl9e2k5p7.apps.googleusercontent.com",
+        "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",
+        "http://localhost:3000/fit-dash"
+    )
+
+    const scopes = ["https://www.googleapis.com/auth/fitness.activity.read profile email openid"];
+    const url = oauth2Client.generateAuthUrl({
+        access_type:"offline",
+        scope: scopes,
+        state: json.toString({
+            callbackUrl: req.body.callbackUrl, 
+            userID: req.body.userid
+        })
+    });
+
+    request(url, (err, response, body) =>{
+        console.log("error", err);
+        console.log(response && response.statusCode);
+        res.send({url});
+    });
 });
+
+app.get("/fit-dash", async (req, res)=>{
+    const queryURL = new urlParse(req.url);
+    const code = queryParse.parse(queryURL.query.code);
+    console.log(code);
+})
 
 app.listen(3000, function () {
     console.log("server started on port 3000");
